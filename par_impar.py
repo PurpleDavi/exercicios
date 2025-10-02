4bb36c879c9251b1cc2bdaae430bcded464e6018
PurpleDavi <davi.bertoldo.almeida@escola.pr.gov.br>
2025-10-02 08:54:55 -0300

    Add files via upload

---
+++ par_impar.py
@@ -0,0 +1,6 @@
+print ("verica se o numero é par ou impar")
+num = int(input("digite um numero:"))
+if num % 2 == 0:
+    print ("o numero é par")
+else:
+    print ("o numero é impar")
---
+++ saudaçao.py
@@ -0,0 +1,8 @@
+print ("ola, tudo bem?")
+print ("seja bem vindo ao meu programa")
+
+nome = input ("qual o seu nome?")
+if nome == "":
+     print ("perdão, não entendi, pode repetir?")
+else:
+    print ("ola", nome, "muito prazer em te cohecer")
